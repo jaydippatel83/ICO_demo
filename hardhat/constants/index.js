e22a441b9fe0c2a0e782b04671d8ab4376d29ace
Jaydippatel <macbook@MacBooks-MacBook-Pro-2.local>
2022-12-14 18:23:38 +0530

    init

---
+++ hardhat/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xb2540c00095e1b18a334b45f61a0a9e35027dd6e";
+// Crypto Devs Token Contract Address: 0x97BC1AAe74fBC8d423bAb4F023A3640797eF42f2
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
